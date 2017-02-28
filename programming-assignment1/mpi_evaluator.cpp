d4865380f59e4de22cd081fbb56b361ba320fe59
Yang Wu <ywu613@gmail.com>
2017-02-28 14:47:21 -0500

    First version of broadcast

--- programming-assignment1/mpi_evaluator.cpp
+++ programming-assignment1/mpi_evaluator.cpp
@@ -8,6 +8,7 @@
 
 #include "mpi_evaluator.h"
 #include "const.h"
+#include <math.h>
 
 void scatter(const int n, double* scatter_values, int &n_local, double* &local_values, int source_rank, const MPI_Comm comm){
     //Implementation
@@ -15,13 +16,13 @@ void scatter(const int n, double* scatter_values, int &n_local, double* &local_v
     MPI_Comm_size(comm, &p);
     MPI_Comm_rank(comm, &rank);
 
-    int num, r;
+    int num = broadcast(n, 0, comm);
+    int r = num % p;
+
     if (rank == source_rank) {
-        r = n % p;
-        n_local = r==0? n/p : n/p+1;
+        n_local = r==0 ? n/p : n/p+1;
         for (int i = 0, start = 0; i < p; i++) {
-            if (i > 0) MPI_Send(&n, 1, MPI_INT, i, i, comm);
-            if (i < r) {
+            if (i < r || r == 0) {
                 MPI_Send(scatter_values + start, n_local, MPI_DOUBLE, i, i, comm);
                 start += n_local;
             } else {
@@ -32,14 +33,10 @@ void scatter(const int n, double* scatter_values, int &n_local, double* &local_v
         local_values = (double*) malloc( n_local * sizeof(double) );
         MPI_Recv(local_values, n_local, MPI_DOUBLE, 0, 0, comm, MPI_STATUS_IGNORE);
     } else {
-        MPI_Recv(&num, 1, MPI_INT, 0, rank, comm, MPI_STATUS_IGNORE);
-        r = num % p;
-        n_local = num / p;
-        if (rank < r) n_local = n_local + 1;
+        n_local = rank<r ? num/p+1 : num/p;
         local_values = (double*) malloc( n_local * sizeof(double) );
         MPI_Recv(local_values, n_local, MPI_DOUBLE, 0, rank, comm, MPI_STATUS_IGNORE);
     }
-    // if (rank == 0) n_local += 1;
     for (int j = 0; j < n_local; j++) {
         std::cout << local_values[j] << " ";
     }
@@ -48,8 +45,26 @@ void scatter(const int n, double* scatter_values, int &n_local, double* &local_v
 
 double broadcast(double value, int source_rank, const MPI_Comm comm){
     //Implementation
+    int p, rank;
+    MPI_Comm_size(comm, &p);
+    MPI_Comm_rank(comm, &rank);
 
-    return 0;
+    int d = ceil(log2(p));
+    bool flag = rank==source_rank ? true : false;
+    for (int i = 0; i < d; i++) {
+        int rank_a = rank ^ int(pow(2, i));
+        if (rank < int(pow(2, i+1)) && rank_a < p) {
+            if (flag) MPI_Send(&value, 1, MPI_DOUBLE, rank_a, 0, comm);
+            else {
+                MPI_Recv(&value, 1, MPI_DOUBLE, rank_a, 0, comm, MPI_STATUS_IGNORE);
+                flag = true;
+            }
+        }
+    }
+
+    std::cout << "Rank = " << rank << " and value is " << value << std::endl;
+
+    return value;
 }
 
 void parallel_prefix(const int n, const double* values, double* prefix_results, const int OP, const MPI_Comm comm){
