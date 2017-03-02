899f8dac89733c9e6c5d95c5c7dc449e0ac08f7d
Yang Wu <ywu613@gmail.com>
2017-03-01 19:33:18 -0500

    Bug fixed. When have a large data, rank 0 cannot send values to itself.

--- programming-assignment1/mpi_evaluator.cpp
+++ programming-assignment1/mpi_evaluator.cpp
@@ -23,7 +23,9 @@ void scatter(const int n, double* scatter_values, int &n_local, double* &local_v
 
     if (rank == source_rank) {
         n_local = r==0 ? n/p : n/p+1;
-        for (int i = 0, start = 0; i < p; i++) {
+        local_values = (double*) malloc( n_local * sizeof(double) );
+        for (int k = 0; k < n_local; k++) local_values[k] = scatter_values[k];
+        for (int i = 1, start = n_local; i < p; i++) {
             if (i < r || r == 0) {
                 MPI_Send(scatter_values + start, n_local, MPI_DOUBLE, i, i, comm);
                 start += n_local;
@@ -32,8 +34,6 @@ void scatter(const int n, double* scatter_values, int &n_local, double* &local_v
                 start += n_local - 1; 
             }
         }
-        local_values = (double*) malloc( n_local * sizeof(double) );
-        MPI_Recv(local_values, n_local, MPI_DOUBLE, 0, 0, comm, MPI_STATUS_IGNORE);
     } else {
         n_local = rank<r ? num/p+1 : num/p;
         local_values = (double*) malloc( n_local * sizeof(double) );
@@ -69,10 +69,13 @@ void parallel_prefix(const int n, const double* values, double* prefix_results, 
     MPI_Comm_rank(comm, &rank);
 
     double local = 0.0;
-    if (OP == PREFIX_OP_SUM)          local = 0.0;
-    else if (OP == PREFIX_OP_PRODUCT) local = 1.0;
+    if (OP == PREFIX_OP_SUM) {
+        local = 0.0;
+    } else if (OP == PREFIX_OP_PRODUCT) {
+        local = 1.0;
+    }
 
-    double total = *(values+n-1);
+    double total = *(prefix_results+n-1);
     double d = log2(p);
     for (int i = 0; i < d; i++) {
         int rank_a = rank ^ int(pow(2, i));
@@ -81,20 +84,30 @@ void parallel_prefix(const int n, const double* values, double* prefix_results, 
             MPI_Send(&total, 1, MPI_DOUBLE, rank_a, 0, comm);
             MPI_Recv(&recv, 1, MPI_DOUBLE, rank_a, 0, comm, MPI_STATUS_IGNORE);
 
-            if (OP == PREFIX_OP_SUM)          total += recv;
-            else if (OP == PREFIX_OP_PRODUCT) total *= recv;
+            if (OP == PREFIX_OP_SUM) {
+                total += recv;
+            } else if (OP == PREFIX_OP_PRODUCT) {
+                total *= recv;
+            }
 
             if (rank_a < rank) {
-                if (OP == PREFIX_OP_SUM)          local += recv;
-                else if (OP == PREFIX_OP_PRODUCT) local *= recv;
+                if (OP == PREFIX_OP_SUM) {
+                    local += recv;
+                } else if (OP == PREFIX_OP_PRODUCT) {
+                    local *= recv;
+                }
             }
         }
     }
 
     if (OP == PREFIX_OP_SUM) {
-        for (int k = 0; k < n; k++) prefix_results[k] += local;
+        for (int k = 0; k < n; k++) {
+            prefix_results[k] += local;
+        }
     } else if (OP == PREFIX_OP_PRODUCT) {
-        for (int k = 0; k < n; k++) prefix_results[k] *= local;
+        for (int k = 0; k < n; k++) {
+            prefix_results[k] *= local;
+        }
     }
 }
 
@@ -104,27 +117,25 @@ double mpi_poly_evaluator(const double x, const int n, const double* constants, 
     MPI_Comm_size(comm, &p);
     MPI_Comm_rank(comm, &rank);
 
-    // First parallel prefix to compute x^i, x^i+1, ..., x^i+n-1
+    // Compute local prefix product, then run parallel prefix
     double* values = (double*) malloc(n*sizeof(double));
     double* prefix = (double*) malloc(n*sizeof(double));
     for (int i = 0; i < n; i++) {
-        if (rank == 0) values[i] = i==0? 1 : values[i-1]*x;
-        else values[i] = i==0? x : values[i-1]*x;
-        prefix[i] = values[i];
+        if (rank == 0 && i == 0) values[i] = 1;
+        else values[i] = x;
+        prefix[i] = i==0? values[i] : prefix[i-1]*values[i];
     }
     parallel_prefix(n, values, prefix, PREFIX_OP_PRODUCT, comm);
 
-    // Second parallel prefix to compute a_i*x^i + a_i+1*x^i+1 + ... + a_i+n-1*x^i+n-1
+    // Compute local prefix sum, then run parallel prefix
     for (int j = 0; j < n; j++) {
-        double temp = constants[j]*prefix[j];
-        values[j] = j==0? temp : values[j-1]+temp;
-        prefix[j] = values[j];
+        values[j] = constants[j] * prefix[j];
+        prefix[j] = j==0? values[j] : prefix[j-1]+values[j];
     }
     parallel_prefix(n, values, prefix, PREFIX_OP_SUM, comm);
 
     double result = prefix[n-1];
     if (rank == p - 1) MPI_Send(&result, 1, MPI_DOUBLE, 0, 0, comm);
-
     if (rank == 0) MPI_Recv(&result, 1, MPI_DOUBLE, p-1, 0, comm, MPI_STATUS_IGNORE);
     return result;
 }
