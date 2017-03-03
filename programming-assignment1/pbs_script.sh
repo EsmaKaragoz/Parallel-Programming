5efb9ff8ff1bfb6891a7cbc99425c3ba5a79cbae
Yang Wu <ywu613@gmail.com>
2017-03-02 19:57:54 -0500

    Add script

--- programming-assignment1/pbs_script.sh
+++ programming-assignment1/pbs_script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #PBS -q class
-#PBS -l node=4:sixcore
+#PBS -l nodes=4:sixcore
 #PBS -l walltime=00:10:00
 #PBS -N ywu613
 
@@ -12,5 +12,5 @@ MPIRUN=/usr/lib64/openmpi/bin/mpirun
 # loop over number of processors (our 4 nodes job can run up to 48)
 for p in 2 4 6 8 10 12 14 16 20 24 28 32 36 40 44 48
 do
-    $MPIRUN -np $p --hostfile $PBS_NODEFILE ./poly-eval -n 5000000 -m 1 -s 6220
+    $MPIRUN -np $p --hostfile $PBS_NODEFILE ./poly-eval -n 5000000 -m 1 -s 2017
 done
