7163ec54218c785c2338efd8be090102c64cd90f
Yang Wu <ywu613@gmail.com>
2017-03-31 17:22:52 -0400

    Add partition function and partition the array

--- programming-assignment2/parallel_sort.cpp
+++ programming-assignment2/parallel_sort.cpp
@@ -20,16 +20,20 @@ void parallel_sort(int* begin, int* end, MPI_Comm comm) {
         return;
     }
 
-    // Get the size of the local array
+    // Get the size of the local array, and the size of the global array
     int local_size = end - begin, total_size;
     MPI_Allreduce(&local_size, &total_size, 1, MPI_INT, MPI_SUM, comm);
+
+    /*********************************************************************
+     *               Broadcast the pivot to each processor               *
+     *********************************************************************/
 
     // Compute the global index of this processor
     int q = total_size / p; // get the quotient
     int r = total_size % p; // get the remainder
     int lo;
     if (rank < r + 1) lo = (q + 1) * rank;
-    else lo = q * rank;
+    else lo = (q + 1) * r + q * (rank - r);
 
     // Set the random seed and uniform integer distribution
     std::default_random_engine generator(6220);
@@ -46,6 +50,18 @@ void parallel_sort(int* begin, int* end, MPI_Comm comm) {
     if (k >= lo && k < lo + local_size)
     	pivot = &(begin + k - lo);
     MPI_Bcast(&pivot, 1, MPI_DOUBLE, root, comm);
+    // MPI_Allreduce(&pivot, &pivot, 1, MPI_INT, MPI_SUM, comm);
+
+    /*********************************************************************
+     *             Partition array locally on each processor             *
+     *********************************************************************/
+    int cutpoint = partition(begin, local_size, pivot);
+    int count[2] = {cutpoint + 1, local_size - cutpoint - 1};
+
+    /*********************************************************************
+     *        Gather the info of two subarrays among all processors      *
+     *********************************************************************/
+
 }
 
 
@@ -53,4 +69,21 @@ void parallel_sort(int* begin, int* end, MPI_Comm comm) {
  *             Implement your own helper functions here:             *
  *********************************************************************/
 
-// ...
+/* Partition an array to two subarrays according to the pivot, and  
+ * return the index of the largest number in the first subarrays */
+int partition(int* begin, int local_size, int pivot) {
+    int i = -1, j = local_size;
+    while (true) {
+        do {
+            i++;
+        } while (&(begin+i) < pivot);
+
+        do {
+            j--;
+        } while (&(begin+j) > pivot);
+
+        if (i >= j) return j;
+
+        std::swap(&(begin+i), &(begin+j));
+    }
+}
