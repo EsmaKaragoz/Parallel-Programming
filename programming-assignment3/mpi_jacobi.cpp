b7711bbfe10b07926bcfb8264baba8ccca61b29b
Yang Wu <ywu613@gmail.com>
2017-04-24 20:05:45 -0400

    add distribute_matrix

--- programming-assignment3/mpi_jacobi.cpp
+++ programming-assignment3/mpi_jacobi.cpp
@@ -36,7 +36,6 @@ void distribute_vector(const int n, double* input_vector, double** local_vector,
 
     if (coords[1] == 0) {
 
-
         // std::cout << coords[0] << " " << coords[1] << std::endl;
         // if (coords[0] == 0 && coords[1] == 0) {
         //     for (int i = 0; i < n; i++) {
@@ -44,7 +43,6 @@ void distribute_vector(const int n, double* input_vector, double** local_vector,
         //     }
         //     std::cout << std::endl;
         // }
-        
 
         // Compute the number of elements to send to each processor
         int* sendcounts = new int[dims[0]];
@@ -68,7 +66,6 @@ void distribute_vector(const int n, double* input_vector, double** local_vector,
         int root_coords[2] = {0, 0};
         MPI_Cart_rank(comm_col, root_coords, &root_rank);
 
-
         // std::cout << root_rank << std::endl;
         // for (int i = 0; i < dims[0]; i++) {
         //     std::cout << sendcounts[i] << " ";
@@ -82,15 +79,14 @@ void distribute_vector(const int n, double* input_vector, double** local_vector,
         // Scatter values to different processors
         MPI_Scatterv(input_vector, sendcounts, displs, MPI_DOUBLE, *local_vector, local_size, MPI_DOUBLE, root_rank, comm_col);
 
-
         // for (int i = 0; i < local_size; i++) {
         //     std::cout << (*local_vector)[i] << " ";
         // }
         // std::cout << std::endl;
 
-
         delete [] sendcounts;
         delete [] displs;
+
     }
 
     // Free the column communicator
@@ -113,6 +109,7 @@ void gather_vector(const int n, double* local_vector, double* output_vector, MPI
     MPI_Cart_sub(comm, remain_dims, &comm_col);
 
     if (coords[1] == 0) {
+
         // Compute the number of elements to send to each processor
         int* recvcounts = new int[dims[0]];
         int* displs = new int[dims[0]];
@@ -139,6 +136,7 @@ void gather_vector(const int n, double* local_vector, double* output_vector, MPI
 
         delete [] recvcounts;
         delete [] displs;
+
     }
 
     // Free the column communicator
@@ -148,7 +146,121 @@ void gather_vector(const int n, double* local_vector, double* output_vector, MPI
 }
 
 void distribute_matrix(const int n, double* input_matrix, double** local_matrix, MPI_Comm comm) {
-    // TODO
+
+    // Get the Cartesian topology information
+    int dims[2], periods[2], coords[2];
+    MPI_Cart_get(comm, 2, dims, periods, coords);
+
+    // if (coords[0] == 0 && coords[1] == 0) {
+    //     std::cout << "===========================" << std::endl;
+    //     std::cout << "Begin distribution" << std::endl;
+    //     std::cout << "Coordinates (" << coords[0] << " " << coords[1] << "): " << std::endl;;
+    //     for (int i = 0; i < n; i++) {
+    //         for (int j = 0; j < n; j++) {
+    //             std::cout << *(input_matrix + i * n + j) << " ";
+    //         }
+    //         std::cout << std::endl;
+    //     }
+    //     std::cout << "===========================" << std::endl;
+    // }
+
+    // Step 1. Distribute rows on processors in the first column
+    // Create column communicators 
+    MPI_Comm comm_col;
+    int remain_dims[2] = {true, false};
+    MPI_Cart_sub(comm, remain_dims, &comm_col);
+
+    // Create a temp matrix to store the values in the first column
+    double* temp_matrix = NULL;
+
+    if (coords[1] == 0) {
+
+        // Compute the number of elements to send to each processor
+        int* sendcounts = new int[dims[0]];
+        int* displs = new int[dims[0]];
+
+        for (int i = 0; i < dims[0]; i++) {
+            sendcounts[i] = n * block_decompose(n, dims[0], i);
+        }
+
+        displs[0] = 0;
+        for (int i = 1; i < dims[0]; i++) {
+            displs[i] = displs[i - 1] + sendcounts[i - 1];
+        }
+
+        // Compute local size for processors in the first column
+        int local_size = sendcounts[coords[0]];
+        temp_matrix = new double[local_size];
+
+        // Get the rank of root in the first column communicator
+        int root_rank;
+        int root_coords[2] = {0, 0};
+        MPI_Cart_rank(comm_col, root_coords, &root_rank);
+
+        // Scatter values to different processors
+        MPI_Scatterv(input_matrix, sendcounts, displs, MPI_DOUBLE, temp_matrix, local_size, MPI_DOUBLE, root_rank, comm_col);
+
+        delete [] sendcounts;
+        delete [] displs;
+
+    }
+
+    // Free the column communicator
+    MPI_Comm_free(&comm_col);
+
+    // Step 2. Distribute submatrix on processors among each row
+    // Create row communicators 
+    MPI_Comm comm_row;
+    remain_dims[0] = false; remain_dims[1] = true;
+    MPI_Cart_sub(comm, remain_dims, &comm_row);
+
+    int local_size_row = block_decompose(n, dims[0], coords[0]);
+    int local_size_col = block_decompose(n, dims[1], coords[1]);
+
+    // Compute the number of elements to send to each processor
+    int* sendcounts = new int[dims[1]];
+    int* displs = new int[dims[1]];
+
+    for (int i = 0; i < dims[1]; i++) {
+        sendcounts[i] = block_decompose(n, dims[1], i);
+    }
+
+    displs[0] = 0;
+    for (int i = 1; i < dims[1]; i++) {
+        displs[i] = displs[i - 1] + sendcounts[i - 1];
+    }
+
+    // Compute local size for processors in the each row
+    local_size_col = sendcounts[coords[1]];
+    (*local_matrix) = new double[local_size_row * local_size_col];
+
+    // Get the rank of root in the first column communicator
+    int root_rank;
+    int root_coords[2] = {0, 0};
+    MPI_Cart_rank(comm_row, root_coords, &root_rank);
+
+    for (int i = 0; i < local_size_row; i++) {
+        MPI_Scatterv((temp_matrix + i * n), sendcounts, displs, MPI_DOUBLE, (*local_matrix + i * local_size_col), local_size_col, MPI_DOUBLE, root_rank, comm_row);
+    }
+
+    delete [] sendcounts;
+    delete [] displs;
+
+    // std::cout << "===========================" << std::endl;
+
+    // std::cout << "Coordinates (" << coords[0] << " " << coords[1] << "): " << std::endl;;
+    // for (int i = 0; i < local_size_row; i++) {
+    //     for (int j = 0; j < local_size_col; j++) {
+    //         std::cout << *(*local_matrix + i * local_size_col + j) << " ";
+    //     }
+    //     std::cout << std::endl;
+    // }
+    // std::cout << "===========================" << std::endl;
+
+    // Free the column communicator
+    MPI_Comm_free(&comm_row);
+
+    return;
 }
 
 
